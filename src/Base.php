879f4ab1570faf90395973f7736de1b19b052093
舒晓贤 <oncwnuNHQO5y24w9RnQQDcK8mMhs@git.weixin.qq.com>
2019-05-07 16:03:19 +0800

    重构快递100

--- src/Base.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace XiaoYun;
-
-class Base
-{
-
-}
--- src/Kuaidi100.php
+++ src/Kuaidi100.php
@@ -6,5 +6,41 @@ namespace XiaoYun;
 
 class Kuaidi100
 {
+    const site = 'http://www.kuaidi100.com'; // 快递100 API接口
 
+    public static function track($postid)
+    {
+        $client = new \GuzzleHttp\Client(['base_uri' => static::site]);
+        $result = $client->request('GET', '/query', [
+            'query' => [
+                'postid' => $postid,
+                'type' => 1,
+                'temp' => '0.' . self::getRandom(16)
+            ]
+        ]);
+        return (String)$result->getBody();
+    }
+
+    public static function detect($postid)
+    {
+        $client = new \GuzzleHttp\Client(['base_uri' => static::site]);
+        $result = $client->request('GET', '/autonumber/auto', [
+            'query' => [
+                'num' => $postid
+            ]
+        ]);
+        return (String)$result->getBody();
+    }
+
+    /*
+     * 获得32位随机数
+     */
+    private static function getRandom($param)
+    {
+        $key = '';
+        for ($i = 0; $i < $param; $i++) {
+            $key .= rand(1, 9);    //生成php随机数
+        }
+        return $key;
+    }
 }
